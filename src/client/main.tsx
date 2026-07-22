c7a408eb740e571d50c7ed32c923e0f4a2a829d3
KoTeuKa404 <135438716+KoTeuKa404@users.noreply.github.com>
2026-07-22 16:30:45 +0300

    feat: route Search Console workspace

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import SearchConsolePage from "./SearchConsolePage";
 import "./styles.css";
 import "./async-jobs.css";
 
+const workspace = new URLSearchParams(window.location.search).get("workspace");
+const content = workspace === "search"
+  ? <SearchConsolePage />
+  : <><App/><a className="global-search-link" href="/?workspace=search">◎ Search Console</a></>;
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <StrictMode>{content}</StrictMode>,
 );
